d53fdbe67d1613b0d645f5ebd8f764bf8fbbf443
Alena Klekovkina <50324020+AlyonaKlekovkina@users.noreply.github.com>
2024-09-26 18:18:02 +0200

    Update solution.py
    Stage 2/4

--- solution.py
+++ solution.py
@@ -1,7 +1,14 @@
-# Write your code here
-string = ''
-inp = input().split(' ')
-for i in inp:
-    letter = chr(int(i) + 97)
-    string += letter
-print(string)
+import string
+
+letters = string.ascii_lowercase
+encoded_message = input().split()
+shift = 3
+decoded_message = ''
+for j in encoded_message:
+    for i in range(len(letters)):
+        if j == letters[i]:
+            if i + shift < len(letters):
+                decoded_message += letters[i + shift]
+            else:
+                decoded_message += letters[i + shift - len(letters)]
+print(decoded_message)
