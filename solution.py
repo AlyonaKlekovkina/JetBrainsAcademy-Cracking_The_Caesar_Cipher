230f1033cd1246ddefabc431b112886d0e0372ce
Alena Klekovkina <50324020+AlyonaKlekovkina@users.noreply.github.com>
2024-10-01 22:44:46 +0200

    Update solution.py
    Stage 4/4

--- solution.py
+++ solution.py
@@ -1,64 +1,48 @@
 import string
 
 
-def secret_word_indexes(secret_word):
-    list_of_indexes = []
-    for i in secret_word:
-        index = letters.index(i)
-        list_of_indexes.append(index)
-    return list_of_indexes
+def get_positions(word, encoded_word):
+    positions_list = []
+    for i in range(len(encoded_word)):
+        first_word_index = letters.index(encoded_word[i])
+        second_word_index = letters.index(word[i])
+        shift =  second_word_index - first_word_index
+        positions_list.append(shift)
+    return positions_list
 
 
-def input_indexes(first_input):
-    list_of_input_indexes = []
-    for i in first_input:
-        index = letters.index(i)
-        list_of_input_indexes.append(index)
-    return list_of_input_indexes
-
-
-def get_shitf(sw_index, inp_index):
-    secret_word_to_check = []
-    count = 0
-    while count < 26:
-        for i in sw_index:
-            if i + count < 26:
-                index = i + count
+def decode_message(len_of_keyword, target_message, the_code):
+    alphabet_lenght = len(letters)
+    start = 0
+    end = len_of_keyword
+    decoded_word = ''
+    while end < len(target_message) + len_of_keyword:
+        substring = target_message[start: end]
+        for i in range(len(the_code)):
+            pos_message = letters.index(substring[i])
+            shift_pos = the_code[i]
+            decoded_letter_position = (pos_message) + (shift_pos)
+            if decoded_letter_position > alphabet_lenght - 1:
+                decoded_letter_position = decoded_letter_position - alphabet_lenght
+                decoded_letter = letters[decoded_letter_position]
             else:
-                index = (i + count) - 26
-            secret_word_to_check.append(index)
-        for i in range(len(inp_index)):
-            start = 0
-            end = len(sw_index)
-            if secret_word_to_check == inp_index[start + i:end + i]:
-                return count
-        count += 1
-        secret_word_to_check.clear()
-
-
-def decode_message(letters, input, shift):
-    decoded_message = ''
-    for j in input:
-        index = letters.index(j) + shift
-        if index < len(letters):
-            decoded_message += letters[index]
-        elif index > len(letters):
-            result = index - len(letters)
-            decoded_message += letters[result]
-    final_message = ''
-    for i in decoded_message:
-        if i == 'x':
-            final_message += ' '
-        else:
-            final_message += i
-    return final_message
+                decoded_letter = letters[decoded_letter_position]
+            if decoded_letter == 'x':
+                decoded_word += ' '
+            else:
+                decoded_word += decoded_letter
+            if len(decoded_word) == len(target_message):
+                break
+        start += len_of_keyword
+        end += len_of_keyword
+    return decoded_word
 
 
 letters = string.ascii_lowercase
-first_input = input().split()
-secret_word = 'butterscotch'
-second_input = input().split()
-sw_index = secret_word_indexes(secret_word)
-inp_index = input_indexes(first_input)
-decoded_shift = get_shitf(sw_index, inp_index)
-print(decode_message(letters, second_input, -decoded_shift))
+len_of_keyword = int(input())
+word = input().split()
+encoded_word = input().split()
+target_message = input().split()
+shifts_list = get_positions(word, encoded_word)
+the_code = shifts_list[:len_of_keyword]
+print(decode_message(len_of_keyword, target_message, the_code))
